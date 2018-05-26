54e4ba712f49c817271bf482d87daffbfa8f248c
maxisun <00354615@uca.edu.sv>
2018-05-25 21:45:28 -0600

    pene duro

--- app/src/main/java/com/example/max00/roomdb/MainActivity.java
+++ app/src/main/java/com/example/max00/roomdb/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //:v
         rv =findViewById(R.id.recicladorpene);
         adapter = new MoviesAdapter(movies);
         rv.setLayoutManager(new LinearLayoutManager(this));
